eb79cfe42dbe1f99fa16b643c7a347d8aff0e1e6
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-06-02 13:47:22 +0530

    update booking details

--- src/Pages/BookDetails.jsx
+++ src/Pages/BookDetails.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function BookDetails() {
   return (
     <div>
-      <h1>BOok Details</h1>
+      <h1>Booking Details</h1>
+      <h3>Schedule</h3>
+      <h5>Movie Title</h5>
+      <p>SPIDERMAN NO WAY HOME</p>
+      <h5>Date</h5>
+      <h5>Mon, 23 Oct 2023</h5>
+      <p>Tiket (3)</p>
     </div>
   )
 }
